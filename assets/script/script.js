0b31a488bb24eb2d556c0ab35895de1b6fe09051
fernando lucas <fernandolucas93@outlook.com>
2024-01-30 22:27:15 -0300

    atualizando js

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,30 +7,21 @@ const cALL = (el)=> document.querySelector(el)
 pizzaJson.map((element, index)=>{
   let pizzaItem = c('.container-modelos ').cloneNode(true)
 
-
+  pizzaItem.setAttribute('data-key', index) // coloca o atributo data-key com a chave index associado
   pizzaItem.querySelector('.imagem img').src = element.img
   pizzaItem.querySelector('.descricao .name').innerHTML = element.name
   pizzaItem.querySelector('.price').innerHTML = `R$ ${element.price.toFixed(2)}`
   pizzaItem.querySelector('.descricao p').innerHTML = element.description
   
+  // pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+  //   console.log(innerHTML = e)
+  // })
+  pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+
+   let key = e.target.closest('.container-modelos').getAttribute('data-key') // volta uma class a cima para celecionar o atributo
+
+    console.log('pizza clicada ' + key)
+  })
   document.querySelector('.pizzaArea').append(pizzaItem)
-    // console.log(pizzaItem)
-  //   let i = document.querySelector('.img-fluid').innerHTML = element.img
-     //('.valor').append(pizzaItem)
-    // console.log(element)
-    //  console.log(index)
-    // console.log(i)
-
-  
    
-
 })
-
-  // const myModalEl = document.getElementById('btn--modal')
-  //   myModalEl.addEventListener('show.bs.hidden', (event) => {
-  //     myModalEl.show(myModalEl)
-  //   })
-
-    const myModal = new bootstrap.Modal('btn--modal', {
-      keyboard: false
-    })
