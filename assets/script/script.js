fb7e1cc084bc964bd1a283fe0bbe6236e80b1579
fernando lucas <fernandolucas93@outlook.com>
2024-02-06 06:53:55 -0300

    atualizando quantidade no modal

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 const c = (el)=> document.querySelector(el)
-const cALL = (el)=> document.querySelector(el)
-
+const cALL = (el)=> document.querySelectorAll(el)
+let modalQt = 1
 
 
   
@@ -13,14 +13,26 @@ pizzaJson.map((element, index)=>{
   pizzaItem.querySelector('.price').innerHTML = `R$ ${element.price.toFixed(2)}`
   pizzaItem.querySelector('.descricao p').innerHTML = element.description
   
-  // pizzaItem.querySelector('a').addEventListener('click', (e)=>{
-  //   console.log(innerHTML = e)
-  // })
   pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+    modalQt = 1
 
-   let key = e.target.closest('.container-modelos').getAttribute('data-key') // volta uma class a cima para celecionar o atributo
-
-    console.log('pizza clicada ' + key)
+    let key = e.target.closest('.container-modelos').getAttribute('data-key') // volta uma class a cima para celecionar o atributo
+    c('.img-modal img').src = pizzaJson[key].img
+    c('.container--modal h5').innerHTML = pizzaJson[key].name
+    c('.container--modal p').innerHTML = pizzaJson[key].description
+    c('.preco .valor--pizza').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+    c('.tam.active').classList.remove('active')
+ 
+    cALL('.tamanhos .tam').forEach((el , tamIndex)=> {
+      if(tamIndex == 2){
+        el.classList.add('active')
+      }
+      el.querySelector('span').innerHTML =  pizzaJson[key].sizes[tamIndex]
+     })
+     
+     c('.qt--num').innerHTML = modalQt
+    
+    
   })
   document.querySelector('.pizzaArea').append(pizzaItem)
    
